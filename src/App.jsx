516055daede540435bafe60788fb888de916733c
GabrielDaHoraSantos <rm95298@estudante.fieb.edu.br>
2025-03-14 09:38:39 -0300

    projeto2f

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class App extends Component{
+
+  render(){
+
+    return (
+      <div>
+        <h3>Aula react Vite</h3>
+        <p>Retomada de Conteudo</p>
+      </div>
+    )
+  }
+}
+
+export default App
